53da2a4820e41db616f5599854ab391a94f447ef
Arjav Choudhary <92720814+Uniquearjav@users.noreply.github.com>
2025-10-08 11:26:56 +0000

    Refactor code structure for improved readability and maintainability

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
-
+import LogoIcon from '../../../public/nt_logo.png'
+import Image from 'next/image'
 export const Logo = () => {
   return (
     /* eslint-disable @next/next/no-img-element */
-    <img
+    <Image
       alt="Nirvatatva Logo"
       className="max-w-[9.375rem] invert dark:invert-0"
-      src="/logo.svg"
+      src={LogoIcon}
     />
   )
 }
